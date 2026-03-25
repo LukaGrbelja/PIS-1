cb9bdc81436b5b79319ff90ce35762ba20dcfb4e
KarloKomsic <karlo.komsic1@proton.me>
2026-03-25 18:04:50 +0100

    Add EmptyMethod to fix Bug
    Bug fix from Issue 1.

--- NewClass.cs
+++ NewClass.cs
@@ -5,10 +5,16 @@
 /// </summary>
 public class Class1
 {
+	// NOTE: This is a constructor.
 	public Class1()
 	{
 		//
 		// TODO: Add constructor logic here
 		//
+	}
+	public void EmptyMethod()  
+	{
+		Console.WriteLine("Code is working!");
+		// Bug fixed.
 	}
 }
